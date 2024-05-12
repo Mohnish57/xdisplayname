413bbd21d5fa2ade3fde0b9b416bbc3d83dd6310
monu <sawlanimohnish@gmail.com>
2024-05-12 09:12:51 +0530

    xdisplayname

---
+++ src/Components/FullName.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./FullName.css";
+function FullName() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  console.log(formData);
+
+  const [output, setOutput] = useState("");
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const result = formData.firstName + " " + formData.lastName;
+    setOutput(result);
+    setFormData({
+      firstName: "",
+      lastName: "",
+    });
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">
+          First Name:
+          <input
+            required
+            name="firstName"
+            type="text"
+            onChange={handleChange}
+            value={formData.firstName}
+          />
+        </label>
+        <label htmlFor="lastName">
+          Last Name:
+          <input
+            required
+            name="lastName"
+            type="text"
+            onChange={handleChange}
+            value={formData.lastName}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <p>Full Name: {output}</p>
+    </div>
+  );
+}
+
+export default FullName;
